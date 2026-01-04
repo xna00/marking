5de27ab08663f10de15fed888865db614c00f31f
xna00 <61122789+xna00@users.noreply.github.com>
2026-01-04 06:53:05 +0000

    Try fixing antivirus

--- extension/extensionHelper.ts
+++ extension/extensionHelper.ts
@@ -8,3 +8,61 @@ window.document.addEventListener("reloadExtension", async () => {
     window.location.reload();
   }, 1000);
 });
+
+const updateBtn = document.getElementById("update_btn");
+
+const directoryId = "directoryId";
+
+const saveFilesInDirectory = async (
+  dirHandle: FileSystemDirectoryHandle,
+  files: Record<string, Uint8Array>
+) => {
+  return Promise.all(Object.entries(files).map(async ([p,fileContent]) => {
+    if (!p) return
+    const filePath = p.split('/')
+    if (filePath.length > 1) {
+      const dir = await dirHandle.getDirectoryHandle(filePath[0], {
+        create: true,
+      });
+      await saveFilesInDirectory(dir, {
+        [filePath.slice(1).join("/")]: fileContent,
+      });
+    } else {
+      const fileHandle = await dirHandle.getFileHandle(p, {
+        create: true,
+      });
+      const writable = await fileHandle.createWritable();
+      await writable.write(fileContent.buffer as ArrayBuffer);
+      await writable.close();
+    }
+  }))
+};
+
+const saveExtensionFiles = async (dirHandle: FileSystemDirectoryHandle) => {
+  const buf = await (await fetch("./extension.zip")).arrayBuffer();
+
+  const files = {};
+  console.log(files);
+  const f = Object.fromEntries(
+    Object.entries(files)
+      .filter(([k, v]) => !k.endsWith("/"))
+      .map(([k, v]) => [k.replace("dist/extension/", ""), v])
+  );
+  await saveFilesInDirectory(dirHandle, f);
+};
+
+updateBtn?.addEventListener("click", async function () {
+  try {
+    const dirHandle = await window.showDirectoryPicker({
+      id: directoryId,
+      mode: "readwrite",
+    });
+    await saveExtensionFiles(dirHandle);
+
+    const event = new CustomEvent("reloadExtension");
+    document.dispatchEvent(event);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
