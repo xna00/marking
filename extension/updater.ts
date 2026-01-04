046b8f465f3e145419a6b27d938967ce06fc0801
xna00 <61122789+xna00@users.noreply.github.com>
2026-01-04 03:57:24 +0000

    Try fixing antivirus

--- extension/updater.ts
+++ extension/updater.ts
@@ -1,8 +1,10 @@
 console.log("updater.ts loaded");
 console.log(chrome);
 
-window.document.addEventListener("reloadExtension", async () => {
-  console.log("Reloading extension...");
-  await chrome.runtime.sendMessage({ action: "reloadExtension" });
-
-});
+// window.document.addEventListener("reloadExtension", async () => {
+//   console.log("Reloading extension...");
+//   await chrome.runtime.sendMessage({ action: "reloadExtension" });
+//   setTimeout(() => {
+//     window.location.reload();
+//   }, 1000);
+// });
