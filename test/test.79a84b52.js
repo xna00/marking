6b8a0bb4a323025bbe74ffe2203f7ae0d9802e7a
xna00 <61122789+xna00@users.noreply.github.com>
2026-01-10 01:19:39 +0000

    Deploying to gh-pages from @ xna00/marking@a566b5a126d7e0441477007fed0f46e668ef9b6d 🚀

---
+++ test/test.79a84b52.js
@@ -0,0 +1,16 @@
+import { imageSrc } from "./imageSrc.ca9e377c.js";
+console.log("main.ts");
+const cardImage = document.getElementById("cardImage");
+const submitBtn = document.getElementById("submitBtn");
+let imageIndex = 0;
+function changeImage() {
+    imageIndex = (imageIndex + 1) % imageSrc.length;
+    cardImage.src = imageSrc[imageIndex];
+}
+changeImage();
+submitBtn.addEventListener("click", () => {
+    changeImage();
+});
+document.addEventListener("customSubmit", () => {
+    submitBtn.click();
+});
