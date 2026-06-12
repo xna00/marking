ae5c835454106ae05e62a1ae81c426f8ab46088f
xna00 <61122789+xna00@users.noreply.github.com>
2026-06-12 09:55:17 +0000

    Deploying to gh-pages from @ xna00/marking@2fcfd7b65724d778c65ef0897a5e9632f642e4e1 🚀

---
+++ multi-AQzt5Dhs.js
@@ -0,0 +1,12 @@
+import{i}from"./imageSrc-DhVgVYj5.js";const n=i.filter(e=>/image\d\.png$/.test(e)),o=n.map(e=>e.replace(/\.png$/,"A.png")),l=n.map(e=>e.replace(/\.png$/,"B.png")),m=document.getElementById("imageContainer"),d=document.getElementById("currentLabel");let t=0;function c(e){m.innerHTML=`
+    <div class="outBox">
+      <div class="imgSection clear">
+        <img src="${o[e]}" />
+      </div>
+    </div>
+    <div class="outBox">
+      <div class="imgSection clear">
+        <img src="${l[e]}" />
+      </div>
+    </div>
+  `,d.textContent=`${e+1} / ${n.length}`}c(0);document.getElementById("prevBtn").onclick=()=>{t=(t-1+n.length)%n.length,c(t)};document.getElementById("nextBtn").onclick=()=>{t=(t+1)%n.length,c(t)};document.getElementById("submitBtn").onclick=()=>{c(t=(t+1)%n.length)};document.addEventListener("customSubmit",()=>{document.getElementById("submitBtn").click()});
